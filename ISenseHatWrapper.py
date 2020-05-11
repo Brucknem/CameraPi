2037baa047cd3cad907a1eeb0af8af5f452f707c
Brucknem <mbruckner94@gmail.com>
2020-05-11 19:12:36 +0200

    Moved sensors to real sense hat and temp to interface

--- ISenseHatWrapper.py
+++ ISenseHatWrapper.py
@@ -66,27 +66,6 @@ class ISenseHatWrapper(Observer):
         cpu = f.readline()
         values = {'Temperature (Chip)': str(int(cpu) / 1000) + ' \'C'}
 
-        try:
-            pixel_list = self.sense.get_pixels()
-            self.sense.clear(0, 0, 255)
-
-            pressure = self.sense.get_pressure
-            humidity = self.sense.get_humidity
-            temperature_humidity = self.sense.get_temperature_from_humidity
-            temperature_pressure = self.sense.get_temperature_from_pressure
-
-            values.update(single_sensor_measurement('Temperature (Pressure)',
-                                                    temperature_pressure))
-            values.update(single_sensor_measurement('Temperature (Humidity)',
-                                                    temperature_humidity))
-            values.update(single_sensor_measurement('Pressure', pressure))
-            values.update(single_sensor_measurement('Humidity', humidity))
-            self.sense.clear()
-            self.sense.set_pixels(pixel_list)
-
-        except Exception as err:
-            logging.exception(err)
-
         return values
 
     def show_ip(self):
--- SenseHatWrapper.py
+++ SenseHatWrapper.py
@@ -1,4 +1,6 @@
-from ISenseHatWrapper import ISenseHatWrapper
+import logging
+
+from ISenseHatWrapper import ISenseHatWrapper, single_sensor_measurement
 
 
 class SenseHatWrapper(ISenseHatWrapper):
@@ -13,3 +15,29 @@ class SenseHatWrapper(ISenseHatWrapper):
         super().__init__()
         from sense_hat import SenseHat
         self.sense = SenseHat()
+
+    def read_sensors(self):
+        """ Overriding """
+        values = super().read_sensors()
+
+        try:
+            pixel_list = self.sense.get_pixels()
+            self.sense.clear(0, 0, 255)
+
+            pressure = self.sense.get_pressure
+            humidity = self.sense.get_humidity
+            temperature_humidity = self.sense.get_temperature_from_humidity
+            temperature_pressure = self.sense.get_temperature_from_pressure
+
+            values.update(single_sensor_measurement('Temperature (Pressure)',
+                                                    temperature_pressure))
+            values.update(single_sensor_measurement('Temperature (Humidity)',
+                                                    temperature_humidity))
+            values.update(single_sensor_measurement('Pressure', pressure))
+            values.update(single_sensor_measurement('Humidity', humidity))
+            self.sense.clear()
+            self.sense.set_pixels(pixel_list)
+
+        except Exception as err:
+            logging.exception(err)
+        return values
