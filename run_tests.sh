80a931b9e1576e87eadae7b7b83e5ee960bba28e
Brucknem <mbruckner94@gmail.com>
2020-05-12 12:22:04 +0200

    Fixed coverage xml reporting

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source camerapiEnv/bin/activate
-pytest -v --cov --cov-config=.coveragerc
+pytest -v --cov --cov-config=.coveragerc --cov-report=xml
 curl --request POST --user coverage:arT3O0ylAHCsr0qevI8c9BPbpzKVPBNr --form "report=@coverage.xml" "http://marcelbruckner.spdns.de:8080/api/projects/camera-pi/external-analysis/session/auto-create/report?format=Cobertura&partition=Unit%20Tests&message=Unit%20Test%20Coverage"
