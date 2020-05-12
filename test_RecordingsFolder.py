5bc2326304794bb0fe5a50c31f85133821cad565
Brucknem <mbruckner94@gmail.com>
2020-05-12 23:22:16 +0200

    Extended recordings folder tests

--- test_RecordingsFolder.py
+++ test_RecordingsFolder.py
@@ -1,9 +1,10 @@
+import os
+
 import pytest
 
 from RecordingsFolder import RecordingsFolder
 
 tests_folder = './tests'
-recordingsFolder = RecordingsFolder(tests_folder)
 
 
 class TestRecordingsFolder:
@@ -15,42 +16,71 @@ class TestRecordingsFolder:
         """
         Test: Singleton class design of RecordingsFolder
         """
-        recordingsfolder = RecordingsFolder(tests_folder)
+        recordings_folder = RecordingsFolder(tests_folder)
 
-        assert recordingsfolder.base_path == tests_folder
+        assert recordings_folder.base_path == tests_folder
 
-        assert recordingsfolder is RecordingsFolder()
-        assert recordingsfolder is RecordingsFolder('./otherTest')
-        assert recordingsfolder.log_dir is RecordingsFolder(
+        assert recordings_folder is RecordingsFolder()
+        assert recordings_folder is RecordingsFolder('./otherTest')
+        assert recordings_folder.log_dir is RecordingsFolder(
             './otherTest').log_dir
-        assert recordingsfolder.log_file_path is RecordingsFolder(
+        assert recordings_folder.log_file_path is RecordingsFolder(
             './otherTest').log_file_path
-        assert recordingsfolder.current_recordings_folder is RecordingsFolder(
+        assert recordings_folder.current_recordings_folder is RecordingsFolder(
             './otherTest').current_recordings_folder
 
     def test_create_new_recording(self):
         """
         Test: Create a new folder for recordings.
         """
-        RecordingsFolder(tests_folder).create_new_recording()
 
-        assert RecordingsFolder().current_recordings_folder is not None
+        assert RecordingsFolder().current_recordings_folder is None
 
-    def get_next_chunk_path(self):
+        for i in range(5):
+            current_recordings_folder = \
+                RecordingsFolder().current_recordings_folder
+            print('Current recordings folder: ', current_recordings_folder)
+
+            RecordingsFolder(tests_folder).create_new_recording()
+
+            assert RecordingsFolder().current_recordings_folder is not None
+
+            new_recordings_folder = \
+                RecordingsFolder().current_recordings_folder
+            print('New recordings folder: ', new_recordings_folder)
+
+            assert current_recordings_folder is not new_recordings_folder
+            assert os.path.exists(new_recordings_folder)
+
+    def test_get_next_chunk_path(self):
         """
         Tests: Return the full path to the current chunk.
         """
-        pass
+        current_chunk_path = RecordingsFolder().get_next_chunk_path()
 
+        for i in range(5):
+            print('Current chunk path: ', current_chunk_path)
 
-# content of conftest.py or a tests file (e.g. in your tests or root directory)
+            next_chunk_path = RecordingsFolder().get_next_chunk_path()
+            print('Next chunk path: ', next_chunk_path)
+
+            assert next_chunk_path is not current_chunk_path
+
+            assert next_chunk_path.endswith('.h264')
+            assert next_chunk_path.startswith(tests_folder)
+            assert next_chunk_path.startswith(
+                RecordingsFolder().current_recordings_folder)
+
+            current_chunk_path = next_chunk_path
+
 
 @pytest.fixture(autouse=True, scope='session')
-def test_suite_cleanup_thing():
+def test_suite_before_and_after_all():
     """
-    Cleanup for the tests folder.
+    Before all and After all for the RecordingsFolder.
     """
     # setup
+    RecordingsFolder(tests_folder)
     yield
     # teardown - put your command here
     import shutil
