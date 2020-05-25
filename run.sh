ddc0734d6e9ded314cec3a8ff2d7604ba1bfb212
Brucknem <mbruckner94@gmail.com>
2020-05-25 22:31:53 +0200

    Added mnt glob as paths

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 source /home/pi/Repositories/Raspberry-Pi/camerapiEnv/bin/activate
 
-/usr/bin/python3 /home/pi/Repositories/Raspberry-Pi/main.py -c 300 -o "/mnt/harddrive/recordings/"
+/usr/bin/python3 /home/pi/Repositories/Raspberry-Pi/main.py -c 300 -o "/mnt/*"
