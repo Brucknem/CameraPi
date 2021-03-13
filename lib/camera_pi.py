e2a6171eda6393526a827df4671068960ae40bce
brucknem <brucknem>
2021-03-13 23:30:45 +0100

    Picamer open with

--- lib/camera_pi.py
+++ lib/camera_pi.py
@@ -1,8 +1,6 @@
 import io
 import time
 
-import picamera
-
 from lib.camera_base import Camera
 import atexit
 
@@ -12,36 +10,22 @@ class Camera(Camera):
     Raspberry Pi camera driver.
     """
 
-    camera = None
-    camera_opened = False
-
     @staticmethod
     def frames():
         """
         inherited
         """
 
-        if not Camera.camera_opened:
-            Camera.camera = picamera.PiCamera()
-            Camera.camera_opened = True
+        with picamera.Camera() as camera:
+            # let camera warm up
+            time.sleep(2)
 
-        # let camera warm up
-        time.sleep(2)
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
 
-        stream = io.BytesIO()
-        for _ in Camera.camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-            # return current frame
-            stream.seek(0)
-            yield stream.read()
-
-            # reset stream for next frame
-            stream.seek(0)
-            stream.truncate()
-
-
-def exit_handler():
-    print("Closing Raspberry Pi camera.")
-    Camera.camera.close()
-
-
-atexit.register(exit_handler)
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
