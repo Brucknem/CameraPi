7fe26bdf995fc6b64a0c8e10694b717ec4a8cffe
brucknem <brucknem>
2021-03-14 18:54:25 +0100

    Password working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
 })
 export class AppComponent {
   title = 'frontend';
+  sidebarOpened = false;
 }
